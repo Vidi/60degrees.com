013f7057807e320bea957aaccabf720b4b123402
Steve Ferrar <steveferrar@gmail.com>
2016-01-27 23:11:39 +0000

    Add FlexSlider script
    Add newest version of the FlexSlider script

--- wordpress/wp-content/themes/60degrees/js/main.js
+++ wordpress/wp-content/themes/60degrees/js/main.js
@@ -12,4 +12,21 @@ $(document).ready(function(){
 	$('html').removeClass('no-js');
 
 
+    // ----- FlexSlider
+    // ---------------------------------------------
+	$('.js-flexslider--carousel').flexslider({
+		animation: 'fade',
+		controlNav: false,
+		directionNav: false,
+		keyboard: true,
+		pauseOnHover: false,
+		start: function(){
+			$('.carousel__image').show();
+			$('.carousel__content').show();
+		},
+		slideshowSpeed:5000,
+		animationSpeed:500
+	});
+
+
 });
